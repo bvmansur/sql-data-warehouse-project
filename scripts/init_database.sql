195eea8de31307b23c7874d08092e03b69d5ba3d
Brian Mansur <bvmansur@icloud.com>
2026-04-06 15:02:54 -0500

    Revise header comment in init_database.sql
    Updated the header comment for clarity and caution.

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,7 +1,7 @@
 /*
-=============================================================
-Create Database and Schemas
-=============================================================
+=======================================================================================
+Create Database and Schemas (CAUTION: This erases existing databases of the same name!)
+=======================================================================================
 Script Purpose:
     This script creates a new database named 'DataWarehouse' after checking if it already exists. 
     If the database exists, it is dropped and recreated. Additionally, the script sets up three schemas 
